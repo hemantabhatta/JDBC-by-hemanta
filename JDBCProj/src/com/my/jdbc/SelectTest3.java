9484288181db91e2565974c18d92abc6e46ffb34
DELL <DELL@DESKTOP-GQK29G8>
2020-08-14 21:24:30 +0530

    recent

--- JDBCProj/src/com/my/jdbc/SelectTest3.java
+++ JDBCProj/src/com/my/jdbc/SelectTest3.java
@@ -1,9 +1,23 @@
 package com.my.jdbc;
 
+import java.util.Scanner;
+
 public class SelectTest3 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc=null;
+		int no=0;
+		try {
+			//read inputs
+			sc=new Scanner(System.in);
+			if (sc!=null) {
+				System.out.println("Enter Employee no.");
+				no=sc.nextInt();
+			}
+			
+		} catch (Exception e) {
+			
+		}
 	}
 
 }
